1f9d8fce87db72663e8fddf52fb40bcd11ad9760
hollowspy <julien.niedzwiecki@neuf.fr>
2018-09-06 08:55:02 +0200

    bdd Online Ok

--- back/bdd/bdd.js
+++ back/bdd/bdd.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql'); 
 
 //Utilisation Mysql en local
-const connection = mysql.createConnection({
-  multipleStatements: true,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'jn8bb11',
-  database : 'artisanats_arts',
-  port : 3306,
-})
-
-//Utilisation via MySQL online sur www.db4free.net
 // const connection = mysql.createConnection({
 //   multipleStatements: true,
-//   host     : 'sql7.freemysqlhosting.net',
-//   user     : 'sql7250961',
-//   password : 'cJCkfU4J8u',
-//   database : 'sql7250961'
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'jn8bb11',
+//   database : 'artisanats_arts',
+//   port : 3306,
 // })
+
+//Utilisation via MySQL online sur www.db4free.net
+const connection = mysql.createConnection({
+  multipleStatements: true,
+  host     : 'sql7.freemysqlhosting.net',
+  user     : 'sql7250961',
+  password : 'cJCkfU4J8u',
+  database : 'sql7250961'
+})
 
 
 connection.connect(function(err){
