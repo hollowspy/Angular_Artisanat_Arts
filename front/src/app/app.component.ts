57ca3f8a5ef6cdc9dbe3446c5e27b65d15913f64
hollowspy <julien.niedzwiecki@neuf.fr>
2018-09-14 09:24:00 +0200

    searchBar ok on desktop

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
  
-  showSearchBar : boolean = false;
+  showSearchBar : boolean = true;
 
   constructor(private router : Router,
   ) { }
